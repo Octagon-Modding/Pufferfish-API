73cb2bf19a8742ec937bfb38b49b5ad448cd2634
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-02-19 18:24:09 +0000

    Not take a name and tool holder as the toolholder contains the name in the identifier

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
@@ -24,7 +24,8 @@ public class ItemRegistry {
         register.register(PufferfishAPI.context.getModEventBus());
     }
 
-    public ItemRegistryHolder registerSword(String name, ToolHolder toolHolder) {
+    public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
+        final String name = toolHolder.identifier.path;
         return new ItemRegistryHolder(register.register(name, () -> new SwordItem(new net.minecraft.world.item.ToolMaterial(
                 TagKey.create(
                         Registries.BLOCK,
