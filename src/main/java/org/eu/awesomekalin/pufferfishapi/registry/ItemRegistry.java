f7cad541307bb06d9c10d52bb06c5dcf046a85e0
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-02-19 20:20:13 +0000

    Add Pickaxe and Axe registration

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
@@ -1,15 +1,13 @@
 package org.eu.awesomekalin.pufferfishapi.registry;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.eu.awesomekalin.pufferfishapi.holders.ItemRegistryHolder;
-import org.eu.awesomekalin.pufferfishapi.holders.SwordHolder;
+import org.eu.awesomekalin.pufferfishapi.holders.ToolHolder;
 import net.minecraft.registry.tag.TagKey;
 
 public class ItemRegistry {
@@ -21,35 +19,103 @@ public class ItemRegistry {
 
     public void register() {}
 
-    public ItemRegistryHolder registerSword(SwordHolder swordHolder) {
-        return new ItemRegistryHolder(Registry.register(Registries.ITEM, Identifier.of(this.modId, swordHolder.name), new SwordItem(
+    public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(Registry.register(Registries.ITEM, Identifier.of(this.modId, toolHolder.name), new SwordItem(
                 new ToolMaterial(TagKey.of(
                         RegistryKeys.BLOCK,
                         Identifier.of(
-                                swordHolder.material.incorrectForBlocksTag.namespace,
-                                swordHolder.material.incorrectForBlocksTag.path
+                                toolHolder.material.incorrectForBlocksTag.namespace,
+                                toolHolder.material.incorrectForBlocksTag.path
                         )
                 ),
-                        swordHolder.material.durability,
-                        swordHolder.material.speed,
-                        swordHolder.material.attackDamageBonus,
-                        swordHolder.material.enchantValue,
+                        toolHolder.material.durability,
+                        toolHolder.material.speed,
+                        toolHolder.material.attackDamageBonus,
+                        toolHolder.material.enchantValue,
                         TagKey.of(
                                 RegistryKeys.ITEM,
                                 Identifier.of(
-                                        swordHolder.material.repairTag.namespace,
-                                        swordHolder.material.repairTag.path
+                                        toolHolder.material.repairTag.namespace,
+                                        toolHolder.material.repairTag.path
                                 )
                         )
                 ),
 
-                swordHolder.attackDamage,
-                swordHolder.attackSpeed,
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
                 new Item.Settings().registryKey(
                         RegistryKey.of(
                                 RegistryKeys.ITEM,
                                 Identifier.of(
-                                        modId, swordHolder.name
+                                        modId, toolHolder.name
+                                )
+                        )
+                )
+        )));
+    }
+
+    public ItemRegistryHolder registerPickaxe(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(Registry.register(Registries.ITEM, Identifier.of(this.modId, "temp"), new PickaxeItem(new ToolMaterial(TagKey.of(
+                RegistryKeys.BLOCK,
+                Identifier.of(
+                        toolHolder.material.incorrectForBlocksTag.namespace,
+                        toolHolder.material.incorrectForBlocksTag.path
+                )
+        ),
+                toolHolder.material.durability,
+                toolHolder.material.speed,
+                toolHolder.material.attackDamageBonus,
+                toolHolder.material.enchantValue,
+                TagKey.of(
+                        RegistryKeys.ITEM,
+                        Identifier.of(
+                                toolHolder.material.repairTag.namespace,
+                                toolHolder.material.repairTag.path
+                        )
+                )
+        ),
+
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
+                new Item.Settings().registryKey(
+                        RegistryKey.of(
+                                RegistryKeys.ITEM,
+                                Identifier.of(
+                                        modId, toolHolder.name
+                                )
+                        )
+                )
+        )));
+    }
+
+    public ItemRegistryHolder registerAxe(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(Registry.register(Registries.ITEM, Identifier.of(this.modId, "temp"), new AxeItem(new ToolMaterial(TagKey.of(
+                RegistryKeys.BLOCK,
+                Identifier.of(
+                        toolHolder.material.incorrectForBlocksTag.namespace,
+                        toolHolder.material.incorrectForBlocksTag.path
+                )
+        ),
+                toolHolder.material.durability,
+                toolHolder.material.speed,
+                toolHolder.material.attackDamageBonus,
+                toolHolder.material.enchantValue,
+                TagKey.of(
+                        RegistryKeys.ITEM,
+                        Identifier.of(
+                                toolHolder.material.repairTag.namespace,
+                                toolHolder.material.repairTag.path
+                        )
+                )
+        ),
+
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
+                new Item.Settings().registryKey(
+                        RegistryKey.of(
+                                RegistryKeys.ITEM,
+                                Identifier.of(
+                                        modId, toolHolder.name
                                 )
                         )
                 )
