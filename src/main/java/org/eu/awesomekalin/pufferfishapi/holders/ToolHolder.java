e426d28d7cf60f4a6d3f03f0ed9d1feda4e595cf
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-02-19 20:24:43 +0000

    Add Pickaxe and Axe registration

--- src/main/java/org/eu/awesomekalin/pufferfishapi/holders/ToolHolder.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/holders/ToolHolder.java
@@ -2,13 +2,13 @@ package org.eu.awesomekalin.pufferfishapi.holders;
 
 import org.eu.awesomekalin.pufferfishapi.util.ToolMaterial;
 
-public class SwordHolder {
+public class ToolHolder {
     public final ToolMaterial material;
     public final float attackDamage;
     public final float attackSpeed;
     public final String name;
 
-    public SwordHolder(ToolMaterial material, float attackDamage, float attackSpeed, String name) {
+    public ToolHolder(ToolMaterial material, float attackDamage, float attackSpeed, String name) {
         this.material = material;
         this.attackDamage = attackDamage;
         this.attackSpeed = attackSpeed;
--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
@@ -5,12 +5,11 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.*;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.eu.awesomekalin.pufferfishapi.PufferfishAPI;
 import org.eu.awesomekalin.pufferfishapi.holders.ItemRegistryHolder;
-import org.eu.awesomekalin.pufferfishapi.holders.SwordHolder;
+import org.eu.awesomekalin.pufferfishapi.holders.ToolHolder;
 
 public class ItemRegistry {
     private final DeferredRegister.Items register;
@@ -25,33 +24,99 @@ public class ItemRegistry {
         register.register(PufferfishAPI.eventBus);
     }
 
-    public ItemRegistryHolder registerSword(SwordHolder swordHolder) {
-        return new ItemRegistryHolder(register.register(swordHolder.name, () -> new SwordItem(new net.minecraft.world.item.ToolMaterial(
+    public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new SwordItem(new ToolMaterial(
                 TagKey.create(
                         Registries.BLOCK,
                         ResourceLocation.fromNamespaceAndPath(
-                                swordHolder.material.incorrectForBlocksTag.namespace,
-                                swordHolder.material.incorrectForBlocksTag.path
+                                toolHolder.material.incorrectForBlocksTag.namespace,
+                                toolHolder.material.incorrectForBlocksTag.path
                         )
                 ),
-                swordHolder.material.durability,
-                swordHolder.material.speed,
-                swordHolder.material.attackDamageBonus,
-                swordHolder.material.enchantValue,
+                toolHolder.material.durability,
+                toolHolder.material.speed,
+                toolHolder.material.attackDamageBonus,
+                toolHolder.material.enchantValue,
                 ItemTags.create(
                         ResourceLocation.fromNamespaceAndPath(
-                                swordHolder.material.repairTag.namespace,
-                                swordHolder.material.repairTag.path
+                                toolHolder.material.repairTag.namespace,
+                                toolHolder.material.repairTag.path
                         )
                 )
         ),
-                swordHolder.attackDamage,
-                swordHolder.attackSpeed,
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
                 new Item.Properties().setId(
                         ResourceKey.create(
                                 Registries.ITEM,
                                 ResourceLocation.fromNamespaceAndPath(
-                                        modId, swordHolder.name
+                                        modId, toolHolder.name
+                                )
+                        )
+                )
+        )));
+    }
+
+    public ItemRegistryHolder registerPickaxe(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new PickaxeItem(new ToolMaterial(
+                TagKey.create(
+                        Registries.BLOCK,
+                        ResourceLocation.fromNamespaceAndPath(
+                                toolHolder.material.incorrectForBlocksTag.namespace,
+                                toolHolder.material.incorrectForBlocksTag.path
+                        )
+                ),
+                toolHolder.material.durability,
+                toolHolder.material.speed,
+                toolHolder.material.attackDamageBonus,
+                toolHolder.material.enchantValue,
+                ItemTags.create(
+                        ResourceLocation.fromNamespaceAndPath(
+                                toolHolder.material.repairTag.namespace,
+                                toolHolder.material.repairTag.path
+                        )
+                )
+        ),
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
+                new Item.Properties().setId(
+                        ResourceKey.create(
+                                Registries.ITEM,
+                                ResourceLocation.fromNamespaceAndPath(
+                                        modId, toolHolder.name
+                                )
+                        )
+                )
+        )));
+    }
+
+    public ItemRegistryHolder registerAxe(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new AxeItem(new ToolMaterial(
+                TagKey.create(
+                        Registries.BLOCK,
+                        ResourceLocation.fromNamespaceAndPath(
+                                toolHolder.material.incorrectForBlocksTag.namespace,
+                                toolHolder.material.incorrectForBlocksTag.path
+                        )
+                ),
+                toolHolder.material.durability,
+                toolHolder.material.speed,
+                toolHolder.material.attackDamageBonus,
+                toolHolder.material.enchantValue,
+                ItemTags.create(
+                        ResourceLocation.fromNamespaceAndPath(
+                                toolHolder.material.repairTag.namespace,
+                                toolHolder.material.repairTag.path
+                        )
+                )
+        ),
+                toolHolder.attackDamage,
+                toolHolder.attackSpeed,
+                new Item.Properties().setId(
+                        ResourceKey.create(
+                                Registries.ITEM,
+                                ResourceLocation.fromNamespaceAndPath(
+                                        modId, toolHolder.name
                                 )
                         )
                 )
