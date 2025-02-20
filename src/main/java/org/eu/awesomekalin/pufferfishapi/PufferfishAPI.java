76eef031970e4f8bf0a13d712b3ecfa698971153
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-02-20 14:04:47 +0000

    Update getMinecraftVersion()

--- src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
@@ -15,7 +15,7 @@ public class PufferfishAPI implements ModInitializer {
     }
 
     public String getMinecraftVersion() {
-        return "1.21.4";
+        return "1.21.1";
     }
 
     public String getModLoader() {
