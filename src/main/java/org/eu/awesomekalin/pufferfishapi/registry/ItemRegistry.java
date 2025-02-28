af8799aae5ac34ebfe8593b4aeed4336a83aacf5
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-02-28 17:14:37 +0000

    Reduce code complexity
    Added getToolMaterial() and getToolProperties() as private methods to make code a bit more readable

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
@@ -30,8 +30,8 @@ public class ItemRegistry {
         register.register(PufferfishAPI.eventBus);
     }
 
-    public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
-        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new SwordItem(new ToolMaterial(
+    private ToolMaterial getToolMaterial(ToolHolder toolHolder) {
+        return new ToolMaterial(
                 TagKey.create(
                         Registries.BLOCK,
                         ResourceLocation.fromNamespaceAndPath(
@@ -49,83 +49,40 @@ public class ItemRegistry {
                                 toolHolder.material.repairTag.path
                         )
                 )
-        ),
-                toolHolder.attackDamage,
-                toolHolder.attackSpeed,
-                new Item.Properties().setId(
-                        ResourceKey.create(
-                                Registries.ITEM,
-                                ResourceLocation.fromNamespaceAndPath(
-                                        modId, toolHolder.name
-                                )
+        );
+    }
+
+    private Item.Properties getToolProperties(ToolHolder toolHolder) {
+        return new Item.Properties().setId(
+                ResourceKey.create(
+                        Registries.ITEM,
+                        ResourceLocation.fromNamespaceAndPath(
+                                modId, toolHolder.name
                         )
                 )
+        );
+    }
+
+    public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new SwordItem(getToolMaterial(toolHolder), toolHolder.attackDamage,
+                toolHolder.attackSpeed,
+                getToolProperties(toolHolder)
         )));
     }
 
     public ItemRegistryHolder registerPickaxe(ToolHolder toolHolder) {
-        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new PickaxeItem(new ToolMaterial(
-                TagKey.create(
-                        Registries.BLOCK,
-                        ResourceLocation.fromNamespaceAndPath(
-                                toolHolder.material.incorrectForBlocksTag.namespace,
-                                toolHolder.material.incorrectForBlocksTag.path
-                        )
-                ),
-                toolHolder.material.durability,
-                toolHolder.material.speed,
-                toolHolder.material.attackDamageBonus,
-                toolHolder.material.enchantValue,
-                ItemTags.create(
-                        ResourceLocation.fromNamespaceAndPath(
-                                toolHolder.material.repairTag.namespace,
-                                toolHolder.material.repairTag.path
-                        )
-                )
-        ),
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new PickaxeItem(getToolMaterial(toolHolder),
                 toolHolder.attackDamage,
                 toolHolder.attackSpeed,
-                new Item.Properties().setId(
-                        ResourceKey.create(
-                                Registries.ITEM,
-                                ResourceLocation.fromNamespaceAndPath(
-                                        modId, toolHolder.name
-                                )
-                        )
-                )
+                getToolProperties(toolHolder)
         )));
     }
 
     public ItemRegistryHolder registerAxe(ToolHolder toolHolder) {
-        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new AxeItem(new ToolMaterial(
-                TagKey.create(
-                        Registries.BLOCK,
-                        ResourceLocation.fromNamespaceAndPath(
-                                toolHolder.material.incorrectForBlocksTag.namespace,
-                                toolHolder.material.incorrectForBlocksTag.path
-                        )
-                ),
-                toolHolder.material.durability,
-                toolHolder.material.speed,
-                toolHolder.material.attackDamageBonus,
-                toolHolder.material.enchantValue,
-                ItemTags.create(
-                        ResourceLocation.fromNamespaceAndPath(
-                                toolHolder.material.repairTag.namespace,
-                                toolHolder.material.repairTag.path
-                        )
-                )
-        ),
+        return new ItemRegistryHolder(register.register(toolHolder.name, () -> new AxeItem(getToolMaterial(toolHolder),
                 toolHolder.attackDamage,
                 toolHolder.attackSpeed,
-                new Item.Properties().setId(
-                        ResourceKey.create(
-                                Registries.ITEM,
-                                ResourceLocation.fromNamespaceAndPath(
-                                        modId, toolHolder.name
-                                )
-                        )
-                )
+                getToolProperties(toolHolder)
         )));
     }
 
