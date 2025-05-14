87dedbc5a285809909a1b245763b6a6640caa1d1
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-05-14 16:28:26 +0100

    Wrap SoundEvent and ArmorMaterial as holder

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/ItemRegistry.java
@@ -1,12 +1,9 @@
 package org.eu.awesomekalin.pufferfishapi.registry;
 
-import com.mojang.datafixers.util.Either;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderOwner;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.food.FoodProperties;
@@ -18,9 +15,6 @@ import org.eu.awesomekalin.pufferfishapi.PufferfishAPI;
 import org.eu.awesomekalin.pufferfishapi.holders.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 public class ItemRegistry {
     private final DeferredRegister.Items register;
@@ -79,53 +73,19 @@ public class ItemRegistry {
     }
 
     private Holder<ArmorMaterial> getArmorMaterial(ArmorHolder armorHolder) {
-        return new Holder<ArmorMaterial> () {
-            @Override
-            public ArmorMaterial value() {
-                return new ArmorMaterial(
-                        armorHolder.defense,
-                        armorHolder.enchantmentValue,
-                        new Holder<SoundEvent>() {
-                            @Override
-                            public SoundEvent value() {
-                                return SoundEventsHolder.getSoundEvent(armorHolder.equipSound);
-                            }
-
-                            @Override public boolean isBound() {return false;}
-                            @Override public boolean is(ResourceLocation resourceLocation) {return false;}
-                            @Override public boolean is(ResourceKey<SoundEvent> resourceKey) {return false;}
-                            @Override public boolean is(Predicate<ResourceKey<SoundEvent>> predicate) {return false;}
-                            @Override public boolean is(TagKey<SoundEvent> tagKey) {return false;}
-                            @Override public boolean is(Holder<SoundEvent> holder) {return false;}
-                            @Override public Stream<TagKey<SoundEvent>> tags() {return Stream.empty();}
-                            @Override public Either<ResourceKey<SoundEvent>, SoundEvent> unwrap() {return null;}
-                            @Override public Optional<ResourceKey<SoundEvent>> unwrapKey() {return Optional.empty();}
-                            @Override public Kind kind() {return null;}
-                            @Override public boolean canSerializeIn(HolderOwner<SoundEvent> holderOwner) {return false;}
-                        },
-                        () -> Ingredient.of(ItemTags.create(
-                                ResourceLocation.fromNamespaceAndPath(
-                                        armorHolder.repairIngredient.namespace,
-                                        armorHolder.repairIngredient.path)
-                        )),
-                        armorHolder.assetId,
-                        armorHolder.toughness,
-                        armorHolder.knockbackResistance
-                );
-            }
-
-            @Override public boolean isBound() {return false;}
-            @Override public boolean is(ResourceLocation resourceLocation) {return false;}
-            @Override public boolean is(ResourceKey<ArmorMaterial> resourceKey) {return false;}
-            @Override public boolean is(Predicate<ResourceKey<ArmorMaterial>> predicate) {return false;}
-            @Override public boolean is(TagKey<ArmorMaterial> tagKey) {return false;}
-            @Override public boolean is(Holder<ArmorMaterial> holder) {return false;}
-            @Override public Stream<TagKey<ArmorMaterial>> tags() {return Stream.empty();}
-            @Override public Either<ResourceKey<ArmorMaterial>, ArmorMaterial> unwrap() {return null;}
-            @Override public Optional<ResourceKey<ArmorMaterial>> unwrapKey() {return Optional.empty();}
-            @Override public Kind kind() {return null;}
-            @Override public boolean canSerializeIn(HolderOwner<ArmorMaterial> holderOwner) {return false;}
-        };
+        return BuiltInRegistries.ARMOR_MATERIAL.wrapAsHolder(new ArmorMaterial(
+                armorHolder.defense,
+                armorHolder.enchantmentValue,
+                BuiltInRegistries.SOUND_EVENT.wrapAsHolder(SoundEventsHolder.getSoundEvent(armorHolder.equipSound)),
+                () -> Ingredient.of(ItemTags.create(
+                        ResourceLocation.fromNamespaceAndPath(
+                                armorHolder.repairIngredient.namespace,
+                                armorHolder.repairIngredient.path)
+                )),
+                armorHolder.assetId,
+                armorHolder.toughness,
+                armorHolder.knockbackResistance
+        ));
     }
 
     public ItemRegistryHolder registerSword(ToolHolder toolHolder) {
