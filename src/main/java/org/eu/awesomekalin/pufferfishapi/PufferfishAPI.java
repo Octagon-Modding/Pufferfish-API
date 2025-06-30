96840345619ceadd92b546314bdecdc57c4913c0
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-06-30 16:02:26 +0100

    Port to 1.21.7

--- src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
@@ -20,7 +20,7 @@ public class PufferfishAPI {
     }
 
     public static String getMinecraftVersion() {
-        return "1.21.6";
+        return "1.21.7";
     }
 
     public static String getModLoader() {
