4c18d02a4f57af25fdaf7a7a4bf3418b41d9dee3
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-07-12 20:45:02 +0100

    Save inventory

--- src/main/java/org/eu/awesomekalin/pufferfishapi/blocks/CustomChestBlock.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/blocks/CustomChestBlock.java
@@ -2,12 +2,21 @@ package org.eu.awesomekalin.pufferfishapi.blocks;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Containers;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import org.eu.awesomekalin.pufferfishapi.registry.BlockRegistry;
 import org.eu.awesomekalin.pufferfishapi.util.ChestSettings;
@@ -58,6 +67,44 @@ public class CustomChestBlock extends BaseEntityBlock {
                     return maxStackSize;
                 }
             };
+        }
+
+        @Override
+        protected void saveAdditional(ValueOutput output) {
+            super.saveAdditional(output);
+            inventory.serialize(output);
+        }
+
+        @Override
+        protected void loadAdditional(ValueInput input) {
+            super.loadAdditional(input);
+            inventory.deserialize(input);
+        }
+
+        public void drops() {
+            SimpleContainer inv = new SimpleContainer(inventory.getSlots());
+            for(int i = 0; i < inventory.getSlots(); i++) {
+                inv.setItem(i, inventory.getStackInSlot(i));
+            }
+
+            Containers.dropContents(this.level, this.worldPosition, inv);
+        }
+
+        @Override
+        public void preRemoveSideEffects(BlockPos pos, BlockState state) {
+            drops();
+            super.preRemoveSideEffects(pos, state);
+        }
+
+        @Nullable
+        @Override
+        public Packet<ClientGamePacketListener> getUpdatePacket() {
+            return ClientboundBlockEntityDataPacket.create(this);
+        }
+
+        @Override
+        public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+            return saveWithoutMetadata(pRegistries);
         }
     }
 }
