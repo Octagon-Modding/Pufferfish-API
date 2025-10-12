986d58d84f5c9407a708076f305e83b88a07d60e
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-10-12 16:13:24 +0300

    Port to 1.21.10

--- src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
@@ -19,7 +19,7 @@ public class PufferfishAPI {
     }
 
     public static String getMinecraftVersion() {
-        return "1.21.9";
+        return "1.21.10";
     }
 
     public static String getModLoader() {
