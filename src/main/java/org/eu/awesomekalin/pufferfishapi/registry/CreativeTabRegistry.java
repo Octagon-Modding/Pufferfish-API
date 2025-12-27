143d1bfb2258b744dec88cdd6d48476adf4f59eb
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-12-27 16:19:57 +0200

    Replace registerTab function with registerTabGlobal
    To not create a breaking change

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/CreativeTabRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/CreativeTabRegistry.java
@@ -49,25 +49,13 @@ public class CreativeTabRegistry {
      * @deprecated As of v1.3.0, this function has been replaced by CreativeTabRegistry.registerTabGlobal
      */
     @Deprecated
-    public void registerTab(String tabId, String titleIdentifier, Object icon, List<Object> tabContents) {
+    public void registerTab(String tabId, String titleIdentifier, ItemRegistryHolder icon, List<ItemRegistryHolder> tabContents) {
         register.register(tabId, () -> CreativeModeTab.builder()
                 .title(Component.translatable(titleIdentifier))
-                .icon(() -> {
-                    if (icon instanceof ItemRegistryHolder itemIcon) {
-                        return itemIcon.data.get().getDefaultInstance();
-                    } else if (icon instanceof BlockRegistryHolder blockIcon) {
-                        return blockIcon.dataItem.get().getDefaultInstance();
-                    }
-                    PufferfishAPI.LOGGER.error("Invalid Creative Mode Tab Icon specified by mod. Crash will occur when opening the creative mode inventory.");
-                    return null;
-                })
+                .icon(() -> icon.data.get().getDefaultInstance())
                 .displayItems((params, output) -> {
                     tabContents.forEach((item) -> {
-                        if (item instanceof ItemRegistryHolder itemData) {
-                            output.accept(itemData.data.get().getDefaultInstance());
-                        } else if (item instanceof BlockRegistryHolder blockData) {
-                            output.accept(blockData.dataItem.get().getDefaultInstance());
-                        }
+                        output.accept(item.data.get().getDefaultInstance());
                     });
                 }).build()
         );
