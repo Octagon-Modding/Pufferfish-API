14d83a9f465d1ea7a80ae6a3e14bae4c858fc68a
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2026-04-14 17:58:03 +0300

    Port to Forge 1.21.5

---
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/holders/BlockRegistryHolder.java
@@ -0,0 +1,22 @@
+package org.eu.awesomekalin.pufferfishapi.holders;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import javax.annotation.Nullable;
+import java.util.function.Supplier;
+
+public class BlockRegistryHolder {
+    public final RegistryObject<Block> data;
+    public final RegistryObject<BlockItem> dataItem;
+    @Nullable public final Supplier<BlockEntityType<?>> dataBlockEntity;
+
+    public BlockRegistryHolder(RegistryObject<Block> block, RegistryObject<BlockItem> item, Supplier<BlockEntityType<?>> blockEntity) {
+        data = block;
+        dataItem = item;
+        dataBlockEntity = blockEntity;
+    }
+}
