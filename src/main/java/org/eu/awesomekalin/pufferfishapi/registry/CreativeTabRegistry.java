81c3503a2a0d1dd62e260228a4146e6fc99cb628
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2026-04-14 17:23:43 +0300

    Add deprecated function for CreativeTabRegistry

--- src/main/java/org/eu/awesomekalin/pufferfishapi/registry/CreativeTabRegistry.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/registry/CreativeTabRegistry.java
@@ -44,4 +44,20 @@ public class CreativeTabRegistry {
                 }).build()
         );
     }
+
+    /**
+     * @deprecated As of v1.3.0, you should be using a list with type object instead of ItemRegistryHolder
+     */
+    @Deprecated
+    public void registerTab(String tabId, String titleIdentifier, ItemRegistryHolder icon, List<ItemRegistryHolder> tabContents) {
+        register.register(tabId, () -> CreativeModeTab.builder()
+                .title(Component.translatable(titleIdentifier))
+                .icon(() -> icon.data.get().getDefaultInstance())
+                .displayItems((params, output) -> {
+                    tabContents.forEach((item) -> {
+                        output.accept(item.data.get().getDefaultInstance());
+                    });
+                }).build()
+        );
+    }
 }
