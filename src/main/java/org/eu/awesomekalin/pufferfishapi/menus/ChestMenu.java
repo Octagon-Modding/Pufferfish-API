e4cceb21b71029af9b4e74194c1ff83556663b35
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2026-04-18 11:21:45 +0300

    Optimise imports and fix mixin

--- src/main/java/org/eu/awesomekalin/pufferfishapi/menus/ChestMenu.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/menus/ChestMenu.java
@@ -2,7 +2,6 @@ package org.eu.awesomekalin.pufferfishapi.menus;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Container;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
