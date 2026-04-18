2931339e6cf9f30280533601902dacef00c610bb
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2026-04-18 11:29:20 +0300

    Optimise imports and fix mixin

--- src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/PufferfishAPI.java
@@ -1,11 +1,9 @@
 package org.eu.awesomekalin.pufferfishapi;
 
-import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(PufferfishAPI.MODID)
