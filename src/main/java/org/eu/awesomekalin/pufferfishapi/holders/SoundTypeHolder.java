4e33878a87d3461ff1cbd977de6d70b1858ac4a9
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2026-04-24 13:26:54 +0300

    Update sound types for 1.21.10 and bump

--- src/main/java/org/eu/awesomekalin/pufferfishapi/holders/SoundTypeHolder.java
+++ src/main/java/org/eu/awesomekalin/pufferfishapi/holders/SoundTypeHolder.java
@@ -124,7 +124,9 @@ public enum SoundTypeHolder {
     RESIN,
     RESIN_BRICKS,
     IRON,
-    DRIED_GHAST;
+    DRIED_GHAST,
+    COPPER_GOLEM_STATUE,
+    SHELF;
 
     public static SoundType getSoundType(SoundTypeHolder soundTypeHolder) {
         switch (soundTypeHolder) {
@@ -479,7 +481,7 @@ public enum SoundTypeHolder {
             case RESIN_BRICKS -> {
                 return SoundType.RESIN_BRICKS;
             }
-            case DRIED_GHAST, IRON, LEAF_LITTER, CACTUS_FLOWER -> {
+            case DRIED_GHAST, IRON, LEAF_LITTER, CACTUS_FLOWER, COPPER_GOLEM_STATUE, SHELF -> {
                 PufferfishAPI.LOGGER.warn("Mod attempted to use sound type that is not available in " + PufferfishAPI.getMinecraftVersion() + ". This may cause issues.");
                 return null;
             }
